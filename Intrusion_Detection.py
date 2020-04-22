f7e6577c6e81452205f40112a0221ab26c51d62f
Niramay_Vaidya <niramay.vaidya@gmail.com>
2020-04-22 21:52:29 +0530

    Made changes to the base code

--- Intrusion_Detection.py
+++ Intrusion_Detection.py
@@ -1,8 +1,14 @@
-#Whenever founds internet connectivity  confirms is it you if not log you off
-#and send pic of intruder to your Pushbullet connected all devices.
-
-#Author :  Vishal Kumar
-#https://github.com/TheBeast007/
+'''
+The program sends a push notification to the Pushbullet Chrome Extension configured by the computer's user
+(ideally, instead of using the Pushbullet Chrome Extension, the Pushbullet mobile application should be used)
+The push notification asks whether the person currently operating the computer is indeed the computer's user
+If the user responds with a no to the push notification, then the intrusion detection control function kicks in
+The control function displays an intruder suspected message
+It then captures a real time picture of the person who is currently operating the computer and sends it via another push
+notification to the user
+It finally logs off from the computer i.e. shuts it down
+On the other hand, if the user responds with a yes, then the program exits
+'''
 
 import cv2
 import numpy as np
@@ -11,53 +17,54 @@ import win32com.client as wincl
 import time
 import os
 
-
-#takes intruder pic
-def intruder_pic():
- cam=cv2.VideoCapture(0)
- s,im=cam.read()
- #cv2.imshow("Test Picture",im)
- cv2.imwrite("Intruder.bmp",im)
-
-
-#intruder suspected message
+# Display the intruder suspected message
 def suspected_message():
-    speak = wincl.Dispatch("SAPI.SpVoice")
-    speak.Speak("Intruder Suspected")
+    speak = wincl.Dispatch('SAPI.SpVoice')
+    speak.Speak('Intruder Suspected')
 
-#Your PushBullet API key
-api_key ="YourPushBulletAPIKeyHere"
-pb =PushBullet(api_key)
-pushMsg =pb.push_note("PYTHON : ","Found Internet Connectivity, is this you? if not message 'No' ")
+# Take the intruder's picture
+def intruder_pic():
+	cam=cv2.VideoCapture(0)
+	s, im = cam.read()
+	# cv2.imshow('Intruder Picture', im)
+	cv2.imwrite("Intruder_Picture.png", im)
 
+# Push the captured image to the Pushbullet Chrome Extension
+def image_send():
+	with open('Intruder_Picture.png', 'rb') as pic:
+		file_data = pb.upload_file(pic, 'Intruder_Picture.png')
+	push = pb.push_file(**file_data)
 
-#pushes captured image to Mobile
-def Image_send():
-    with open("Intruder.bmp", "rb") as pic:
-        file_data = pb.upload_file(pic, "Intruder.bmp")
-
-    push = pb.push_file(**file_data)
-
-#log off PC if Intruder Suspected
-def logOff():
-    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
-
-
-
-#Controller
-def Control():
-    while True:
-     val =pb.get_pushes()
-     action =val[0]['body']
-     print(action)
-     if action=='No' or 'no':
-        suspected_message()
-        intruder_pic()
-        Image_send()
-        time.sleep(15)
-        logOff()
-     else:
-        pass
+# Log off from the computer if intruder is suspected (shuts down the computer)
+def log_off():
+	os.system('rundll32.exe powrprof.dll,SetSuspendState 0,1,0')
 
 
-Control()
+
+# The intrusion detection control function
+def control():
+	while True:
+		val = pb.get_pushes()
+		try:
+			action = val[0]['body']
+		except KeyError:
+			print('Could not identify body of the action')
+		print(action)
+		if action.lower() == 'no':
+			suspected_message()
+			intruder_pic()
+			image_send()
+			time.sleep(15)
+			log_off()
+		elif action.lower() == 'yes':
+			break
+		else:
+			pass
+
+if __name__ == '__main__':
+	# User's PushBullet API key
+	api_key = '<api_key>'
+	pb = PushBullet(api_key)
+	push_msg = pb.push_note("PYTHON : ", "Found Internet Connectivity, is this you? If not, message 'No' ")
+
+	control()
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Intrusion_Detection
 
-Whenever founds internet connectivity  confirms is it you. <br />
-If not log you off and send you image of intruder. <br />
+The program sends a push notification to the Pushbullet Chrome Extension configured by the computer's user (ideally, instead of using the Pushbullet Chrome Extension, the Pushbullet mobile application should be used, but for proof of concept, the chrome extension is good enough)
+<br/>
+The push notification asks whether the person currently operating the computer is indeed the computer's user
+<br/>
+If the user responds with a no to the push notification, then the intrusion detection control function kicks in
+<br/>
+The control function displays an intruder suspected message
+<br/>
+It then captures a real time picture of the person who is currently operating the computer and sends it via another push notification to the user
+<br/>
+It finally logs off from the computer i.e. shuts it down
+<br/>
+On the other hand, if the user responds with a yes, then the program exits
 
 ------ 
-![image](https://raw.githubusercontent.com/the-vishal/Intrusion_Detection/master/Intruder.png)
-
+![image](https://raw.githubusercontent.com/NiramayVaidya/Intrusion_Detection/blob/master/Intruder_Picture.png)
